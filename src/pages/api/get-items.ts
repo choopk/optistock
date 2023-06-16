e694f3c10002f68eb81bceb6edb254caf72d8ce3
Mohd Fidzrin <mohdfidzrin@gmail.com>
2023-06-16 21:29:53 +0800

    - used prettier formatter

--- src/pages/api/get-items.ts
+++ src/pages/api/get-items.ts
@@ -1,39 +1,37 @@
-import {Category, PrismaClient} from "@prisma/client"
-import type {NextApiRequest, NextApiResponse} from 'next'
+import { Category, PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 type Item = {
-    id: number,
-    name: string,
-    sku: string,
-    description?: string | null,
-    category: Category,
-    quantity: number,
-    createdAt: Date,
-    updatedAt: Date
-}
+  id: number;
+  name: string;
+  sku: string;
+  description?: string | null;
+  category: Category;
+  quantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Item[]>
+  req: NextApiRequest,
+  res: NextApiResponse<Item[]>
 ) {
-    const query = req.query
-    const keyValue = query.sort?.toString().split(",")
-    const sort = keyValue ? Object.fromEntries([keyValue]) : undefined
-    const skip = query.skip ? parseInt(query.skip.toString()) : undefined
-    const take = query.take ? parseInt(query.take.toString()) : undefined
+  const query = req.query;
+  const keyValue = query.sort?.toString().split(",");
+  const sort = keyValue ? Object.fromEntries([keyValue]) : undefined;
+  const skip = query.skip ? parseInt(query.skip.toString()) : undefined;
+  const take = query.take ? parseInt(query.take.toString()) : undefined;
 
-    const items = await prisma.item.findMany({
-        skip,
-        take,
-        orderBy: [
-            sort
-        ],
-        include: {
-            category: true
-        }
-    })
+  const items = await prisma.item.findMany({
+    skip,
+    take,
+    orderBy: [sort],
+    include: {
+      category: true,
+    },
+  });
 
-    res.status(200).json(items)
+  res.status(200).json(items);
 }
