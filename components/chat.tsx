926eca3030e85703399020ccc2f77c0f0786caf6
Choo Jian Cheng <ext-choo.jian.cheng@solenovo.fi>
2023-06-16 21:45:08 +0800

    Used shadcn's scrollArea, added auto scrolling feature on new dialogue

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Button } from './button'
 import { type ChatGPTMessage, ChatLine, LoadingChatLine } from './chat-line'
 import { useCookies } from 'react-cookie'
+import { ScrollArea } from './ui/scroll-area'
 
 const COOKIE_NAME = 'nextjs-example-ai-chat-gpt3'
 
-// default first message to display in UI (not necessary to define the prompt)
 export const initialMessages: ChatGPTMessage[] = [
   {
     role: 'assistant',
@@ -49,6 +49,7 @@ export function Chat() {
   const [input, setInput] = useState('')
   const [loading, setLoading] = useState(false)
   const [cookie, setCookie] = useCookies([COOKIE_NAME])
+  const endOfMessagesRef = useRef<null | HTMLDivElement>(null)
 
   useEffect(() => {
     if (!cookie[COOKIE_NAME]) {
@@ -57,6 +58,14 @@ export function Chat() {
       setCookie(COOKIE_NAME, randomId)
     }
   }, [cookie, setCookie])
+
+  useEffect(() => {
+    if (endOfMessagesRef.current) {
+      if (loading) {
+        endOfMessagesRef.current.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+  }, [messages, loading])
 
   // send message to API /api/chat endpoint
   const sendMessage = async (message: string) => {
@@ -113,21 +122,20 @@ export function Chat() {
     }
   }
 
-return (
+  return (
     <div className="rounded-2xl border-zinc-100 lg:border lg:p-6 h-[500px] flex flex-col overflow-y-auto">
-      <div className="flex-grow overflow-auto">
+      <ScrollArea className="flex-grow">
         {messages.map(({ content, role }, index) => (
           <ChatLine key={index} role={role} content={content} />
         ))}
-
         {loading && <LoadingChatLine />}
-
         {messages.length < 2 && (
           <span className="mx-auto flex flex-grow text-gray-600 clear-both">
             Type a message to start the conversation
           </span>
         )}
-      </div>
+        <div ref={endOfMessagesRef} />
+      </ScrollArea>
       <InputMessage
         input={input}
         setInput={setInput}
