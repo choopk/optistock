282adf90599e9c8b6dc4a28a2a0213bcbcb49a3f
Choo Jian Cheng <ext-choo.jian.cheng@solenovo.fi>
2023-06-17 10:12:56 +0800

    Remove session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import { Inter } from "@next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
 import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css";
@@ -22,14 +21,11 @@ export default function App({
           font-family: ${inter.style.fontFamily};
         }
       `}</style>
-
-      <SessionProvider session={session}>
-        <ToastContainer />
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </SessionProvider>
+      <ToastContainer />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
